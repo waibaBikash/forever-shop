8f4f5971953ce686bbb282830df87183684b5e29
waibaBikash <kavrelibikash@gmail.com>
2026-03-16 21:38:10 +1100

    feat(client): enhance Tailwind CSS integration with updated configurations and styles

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-green-500">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
