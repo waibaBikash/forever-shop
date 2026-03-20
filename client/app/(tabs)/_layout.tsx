264ebabde2c04a3edc239eaffb8fa489bc651a59
waibaBikash <kavrelibikash@gmail.com>
2026-03-20 23:25:48 +1100

    style(client): format tabBarIcon functions for better readability

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -7,16 +7,20 @@ export default function TabLayout() {
   return (
     <Tabs>
        <Tabs.Screen name='index' options={{
-         tabBarIcon: ({ color, focused })=> <Ionicons name={focused ? 'home' : 'home-outline'} size=  {26} color={color}/>
+         tabBarIcon: ({ color, focused })=>
+           <Ionicons name={focused ? 'home' : 'home-outline'} size=  {26} color={color}/>
        }}/>
        <Tabs.Screen name='cart' options={{
-         tabBarIcon: ({ color, focused })=> <Feather name={focused ? 'shopping-cart' : 'shopping-cart'} size={26} color={color}/>
+         tabBarIcon: ({ color, focused })=> 
+         <Feather name={focused ? 'shopping-cart' : 'shopping-cart'} size={26} color={color}/>
        }}/>
        <Tabs.Screen name='favorite' options={{
-         tabBarIcon: ({ color, focused })=> <Ionicons name={focused ? 'heart' : 'heart-outline'} size={26} color={color}/>
+         tabBarIcon: ({ color, focused })=>
+           <Ionicons name={focused ? 'heart' : 'heart-outline'} size={26} color={color}/>
        }}/>
        <Tabs.Screen name='profile' options={{
-         tabBarIcon: ({ color, focused })=> <Ionicons name={focused ? 'person' : 'person-outline'} size={26} color={color}/>
+         tabBarIcon: ({ color, focused })=>
+           <Ionicons name={focused ? 'person' : 'person-outline'} size={26} color={color}/>
        }}/>
     </Tabs>
   )
